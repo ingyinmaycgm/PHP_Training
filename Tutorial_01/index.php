79ec5d14efb5a9f6aa2907a7b7c45af838df204a
ingyinmaycgm <ingyinmay@cgm-myanmar.com>
2023-10-24 16:46:47 +0630

    add_tutorial1_2023_10_24

---
+++ Tutorial_01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+function drawChessBorad($rows, $cols) {
+    // your code here
+}
+
+// drawChessBorad(8, 8); // output => 8 rows and 8 columns chess borad.
+// drawChessBorad(5, 2); // output => 5 rows and 2 columns chess borad.
+// drawChessBorad(0, 1); // output => $rows parameter must be greater than 0.
+// drawChessBorad(1, 0); // output => $cols parameter must be greater than 0.
+// drawChessBorad(0, 0); // output => $rows and $cols parameters must be greater than 0.
+// drawChessBorad('myrow', 'mycols'); // output => $rows and $cols parameters must be number.
+// drawChessBorad('myrow', 5); // output => $rows parameter must be number.
+// drawChessBorad(5, 'mycols'); // output => $cols parameter must be number.
+// drawChessBorad(0, 'mycols'); // output => $rows parameter must be greater than 0.$cols parameter must be number.
+// drawChessBorad('myrow', 0); // output => $rows parameter must be number.$cols parameter must be greater than 0.
