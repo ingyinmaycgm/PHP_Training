f8e51d97a0a2f863d93f5b422efcbc1c66226442
ingyinmaycgm <ingyinmay@cgm-myanmar.com>
2023-10-25 08:07:19 +0630

    add_tutorial2_2023_10_25

---
+++ Tutorial_02/index.php
@@ -0,0 +1,38 @@
+<?php
+
+function makeDiamondShape($row) {
+    # your code here
+}
+
+// Result
+
+// makeDiamondShape(1);
+// output
+//  *
+
+// makeDiamondShape(3);
+// output
+//  *
+// ***
+//  *
+
+// makeDiamondShape(5);
+// output
+//   *
+//  ***
+// *****
+//  ***
+//   *
+
+// makeDiamondShape(6);
+// output
+// $row parameter must be odd number.
+
+// makeDiamondShape(2);
+// output
+// $row parameter must be odd number.
+
+makeDiamondShape('three');
+// output
+// $row parameter must be number.
+
